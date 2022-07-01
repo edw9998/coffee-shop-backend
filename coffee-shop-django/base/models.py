86fe2471b55ba04c7d9163ff16e78d3e20d9068b
EdwardK <62472600+edw9998@users.noreply.github.com>
2022-07-01 15:54:23 +0700

    Update models.py

--- coffee-shop-django/base/models.py
+++ coffee-shop-django/base/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 # Create your models here.
 class Item(models.Model):
-    Name = models.CharField(max_length=1000)
-    E-mail= models.CharField(max_length=1000)
-    Password = models.Charfield(max_length=1000)
-    Created = models.DateTimeField(auto_now_add=True)
+    HTTP_Method = models.CharField(max_length=1000)
+    URL_Endpoint = models.CharField(max_length=1000)
+    Request_Body = models.Charfield(max_length=1000)
+    Response_Body = models.Charfield(max_length=1000)
+    Created_At = models.DateTimeField(auto_now_add=True)
